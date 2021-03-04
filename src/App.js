4977f80141c68f2cdc8ab524c1ed49fd6927b6a2
danniellabot <danniellanas@gmail.com>
2021-03-04 00:37:41 +0000

    Adding convert and timer resets

--- src/App.js
+++ src/App.js
@@ -1,54 +1,45 @@
-import React, { useState } from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+import React from 'react';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
+// import Dropdown from './components/Dropdown';
+import Translate from './components/Translate'
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework',
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers',
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components',
-  },
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework',
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite JS library among engineers',
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components',
+//   },
+// ];
 
-const options = [
-  {
-    label: 'The Color Red',
-    value: 'red',
-  },
-  {
-    label: 'The Color Green',
-    value: 'green',
-  },
-  {
-    label: 'A Shade of Blue',
-    value: 'blue',
-  },
-];
+// const options = [
+//   {
+//     label: 'The Color Red',
+//     value: 'red',
+//   },
+//   {
+//     label: 'The Color Green',
+//     value: 'green',
+//   },
+//   {
+//     label: 'A Shade of Blue',
+//     value: 'blue',
+//   },
+// ];
 
-export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
+const App = () => {
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        />
-      ) : null}
+      <Translate />
     </div>
   );
 };
+
+export default App
